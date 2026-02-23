9c0f778a1a242d5afe7c2fdcd1250fd717966046
Nandan B V <nandanbv6362@gmail.com>
2026-02-23 23:55:21 +0530

    Remove commented-out code and unused imports

--- src/streaming/producer.py
+++ src/streaming/producer.py
@@ -1,11 +1,1 @@
-# we are going to first load the log to queue and then by using the consumer function we will take out the log 
-import threading
-from queue import Queue 
-my_queue=
-thread1.join()
-thread2.start()
-thread2.join()
-
-
-
 
